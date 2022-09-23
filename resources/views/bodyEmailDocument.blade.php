02211efd30f2fbb127540d6ae0269ff571cccd1a
Wachid Nurchoirudin <wachidnf1998@gmail.com>
2022-09-23 10:42:20 +0700

    update body Email

--- resources/views/bodyEmailDocument.blade.php
+++ resources/views/bodyEmailDocument.blade.php
@@ -1,5 +1,50 @@
 <table>
     <tr>
-        <td><?=$body?></td>
+        <td>Dear <?=$email_to[0]?>,</td>
     </tr>
+    <tr></tr>
+    <tr>
+        <td>Kepada Bapak/ Ibu, 
+            @if (count($email_to) > 1)
+                @foreach ($email_to as $key => $value)
+                    @if ($key == 0)
+                    <?=$value?>
+                    @else
+                    , <?=$value?>
+                    @endif
+                @endforeach
+            @else
+                <?=$email_to[0]?>
+            @endif
+            telah mengirimkan dokumen sebagai berikut :
+        </td>
+    </tr>
+    <tr></tr>
+    <tr>
+        <td>Nomor Dokumen : <?=$doc_no?></td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr>
+        <td>
+            <i>Jika Anda bukan penerima yang ditujukan diatas, Anda tidak berhak untuk mengakses informasi pada lampiran dan harus segera menghapus email ini.</i>
+        </td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr>
+        <td>
+            <i>Simpan alamat email ini pada Address Book email Anda agar tidak masuk kedalam Junk Mail / Spam.</i>
+        </td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+    <tr>
+        <td>Terima Kasih,</td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr><td><?=$pt_name != '' ? strtoupper($pt_name) : 'ADMIN SI ARDI'?></td></tr>
 </table>
